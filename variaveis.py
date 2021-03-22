2921be4cc4b11e492b48ff1a956c4b79cd881bc1
maestriadados <78383295+maestriadados@users.noreply.github.com>
2021-03-22 13:52:54 -0300

    Add files via upload
    Código executado para a demonstração das variáveis na linguagem Python

---
+++ variaveis.py
@@ -0,0 +1,81 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+#PYTHON -- PYTHON -- PYTHON -- PYTHON
+#O canal @maestriadados vai disponibilizar diversos conteúdos 
+#sobre a linguagem de programação mais usada atualmente.
+#Uma linguagem que viabiliza desde a análise dos dados ao 
+#fluxo de modelagem avançada e hiperparametrizada. 
+#Bora lá meus chegados??!!!
+#Estudando o Python!
+
+#O Que são variáveis?
+
+#Variável é um recurso das linguagens de programação, dedicado a armazenar
+#informações em memória, para utilização dos valores que foram atribuídos 
+#no fluxo de lógica aplicado dentro do programa.
+
+#O PYTHON habilita dois principais recursos nos trabalhos com variáveis, 
+#NOMEAÇÃO e ATRIBUÍÇÃO
+
+#NOMEAÇÃO é a possibilidade de dar nomes as variáveis que são criadas, 
+#facilitando o conhecimento e a descritiva no código,
+#no qual, em PYTHON, existe a necessidade de iniciar essa nomenclatura com 
+#uma letra minúscula. Existem alguns padrões bastante usados, 
+#provenientes de boas práticas e metodologias
+
+#EXEMPLOS
+#mensagemHelloWorld (Variável que define uma mensagem de texto)
+#numeroExemplo (Variável que define um valor numérico)
+#pi (Variável que define um valor decimal)
+
+#ATRIBUÍÇÃO é o recurso de atribuir, setar, valor a uma variável definida.
+#Podendo ser de diversos tipos, STRING, NUMÉRICO, DECIMAL.
+
+#variável STRING
+mensagemHelloWorld = 'Olá mundo! Python é demais!!'
+
+#variável NÚMERO
+numeroExemplo = 10000
+
+#variável DECIMAL
+pi = 3.141592653589931
+
+#variável BOOLEANA
+validaSim = True
+
+#variável LISTA
+listaExemplo = [0,1,2,3,4,5]
+
+#variável OBJETO
+class Classe1 (object):
+    pass
+
+objetoExemplo = Classe1()
+
+#Mostrando o valor atribuído de cada variável no programa
+print(mensagemHelloWorld)
+print(numeroExemplo)
+print(pi)
+print(validaSim)
+print(listaExemplo)
+print(Classe1)
+
+#Mostrando o tipo de cada variável no programa
+print(type(mensagemHelloWorld))
+print(type(numeroExemplo))
+print(type(pi))
+print(type(validaSim))
+print(type(listaExemplo))
+print(type(Classe1))
+
+#Recaptulando... Usamos nessa demonstração a função PRINT, para realizar
+#os OUTPUTS dos valores atribuídos nas variáveis.
+#E também a função TYPE, que descreve o tipo de cada variável possível
+#dentro do programa.
+
+
